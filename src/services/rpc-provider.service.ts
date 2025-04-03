73cfa463f36cd80dfb1b8eea0f6d5fd299fb89ba
SebasAran16 <sebastianarango201316@hotmail.com>
2025-04-03 17:24:00 +0200

    Added secondary security RPC Endpoints

--- src/services/rpc-provider.service.ts
+++ src/services/rpc-provider.service.ts
@@ -3,15 +3,15 @@ import { getPublicClient } from '../provider/client';
 import { SupportedChain } from '../provider/types';
 
 const chainToPublicRPCUrls: Record<number, string[]> = {
-    [mainnet.id]: [mainnet.rpcUrls.default.http[0]],
-    [polygon.id]: [polygon.rpcUrls.default.http[0]],
-    [celo.id]: [celo.rpcUrls.default.http[0]],
-    [bsc.id]: [bsc.rpcUrls.default.http[0]],
-    [fantom.id]: [fantom.rpcUrls.default.http[0]],
-    [avalanche.id]: [avalanche.rpcUrls.default.http[0]],
-    [arbitrum.id]: [arbitrum.rpcUrls.default.http[0]],
-    [base.id]: [base.rpcUrls.default.http[0]],
-    [sepolia.id]: [sepolia.rpcUrls.default.http[0]],
+    [mainnet.id]: [mainnet.rpcUrls.default.http[0], 'https://eth.llamarpc.com', 'https://eth.drpc.org'],
+    [polygon.id]: [polygon.rpcUrls.default.http[0], 'https://polygon-pokt.nodies.app', 'https://polygon-bor-rpc.publicnode.com'],
+    [celo.id]: [celo.rpcUrls.default.http[0], 'https://celo.drpc.org'],
+    [bsc.id]: [bsc.rpcUrls.default.http[0], 'https://bsc-mainnet.public.blastapi.io', 'https://bsc.drpc.org'],
+    [fantom.id]: [fantom.rpcUrls.default.http[0], 'https://fantom-pokt.nodies.app', 'https://rpc.fantom.network'],
+    [avalanche.id]: [avalanche.rpcUrls.default.http[0], 'https://avalanche-c-chain-rpc.publicnode.com', 'https://avalanche.drpc.org'],
+    [arbitrum.id]: [arbitrum.rpcUrls.default.http[0], 'https://endpoints.omniatech.io/v1/arbitrum/one/public', 'https://arbitrum.drpc.org'],
+    [base.id]: [base.rpcUrls.default.http[0], 'https://base-rpc.publicnode.com', 'https://base.llamarpc.com'],
+    [sepolia.id]: [sepolia.rpcUrls.default.http[0], 'https://ethereum-sepolia-rpc.publicnode.com', 'https://eth-sepolia.public.blastapi.io'],
 };
 
 export class RpcProviderService {
