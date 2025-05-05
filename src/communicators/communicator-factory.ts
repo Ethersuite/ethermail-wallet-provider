f9c2282173994049d7afcd3760f5743d8e726e5f
SebasAran16 <sebastianarango201316@hotmail.com>
2025-05-05 10:17:44 +0200

    fix:iFrame communication when ethermail provider

--- src/communicators/communicator-factory.ts
+++ src/communicators/communicator-factory.ts
@@ -3,7 +3,7 @@ import { SocketCommunicator } from './socket-communicator';
 
 export class CommunicatorFactory {
   static create(options: any) {
-    if (window?.parent !== window) {
+    if (window?.ethereum?.isEtherMail || window?.parent !== window) {
       return new iframeCommunicator(options);
     } else {
       return new SocketCommunicator(options);
