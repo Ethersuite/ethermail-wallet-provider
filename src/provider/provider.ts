26997ad625fa71c81484b559a1b5fdd2fd9e0b89
SebasAran16 <sebastianarango201316@hotmail.com>
2025-05-27 10:34:45 +0200

    Removed RPC URL Check producing unexpected behavior

--- src/provider/provider.ts
+++ src/provider/provider.ts
@@ -54,12 +54,6 @@ export class EtherMailProvider implements EIP1193Provider {
     });
 
     this._eventEmitter.emit('connect', { chainId: chainId.toString() });
-
-    if (!rpcUrl) {
-      this._rpcProviderService.getPublicRpcUrlForChain(this._chainId).then((rpcUrl) => {
-        this._rpcUrl = rpcUrl;
-      });
-    }
   }
 
   public get chainId() {
